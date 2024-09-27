a7bd734257d87e2de1f7236642674afe2cbf79e9
Light Play <lightplay456@gmail.com>
2024-09-27 15:26:37 +0300

    #

---
+++ MasterAnalyticsDeadByDaylight/Utils/Enum/CalculationOrOutput.cs
@@ -0,0 +1,8 @@
+﻿namespace MasterAnalyticsDeadByDaylight.Utils.Enum
+{
+    public enum CalculationOrOutput
+    {
+        Calculation = 0,
+        Output = 1,
+    }
+}
---
+++ MasterAnalyticsDeadByDaylight/Utils/Enum/TypeTime.cs
@@ -0,0 +1,10 @@
+﻿namespace MasterAnalyticsDeadByDaylight.Utils.Enum
+{
+    public enum TypeTime
+    {
+        Year = 1,
+        Month = 2,
+        Week = 3,
+        Day = 4,
+    }
+}
