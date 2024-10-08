b477e69284b325b76c3c2dcbdaa57d06f900ad08
Light Play <lightplay456@gmail.com>
2024-10-08 13:24:03 +0300

    #

---
+++ MasterAnalyticsDeadByDaylight/Utils/Enum/TypeAssociation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterAnalyticsDeadByDaylight.Utils.Enum
+{
+    public enum TypeAssociation
+    {
+        General = 0,
+        Killer = 1,
+        Survivor = 2
+    }
+}
