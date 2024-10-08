eca725055da21e4a485ebab567e2abd6f676ec69
Light Play <lightplay456@gmail.com>
2024-10-08 13:26:03 +0300

    Создание окна для настроек.

---
+++ MasterAnalyticsDeadByDaylight/MVVM/View/Windows/AppWindow/SettingsWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using MasterAnalyticsDeadByDaylight.MVVM.ViewModel.WindowsViewModels;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MasterAnalyticsDeadByDaylight.MVVM.View.Windows.AppWindow
+{
+    /// <summary>
+    /// Логика взаимодействия для SettingsWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+            StateChanged += MainWindowStateChangeRaised;
+            DataContext = new SettingsViewModel();
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CommandBinding_Executed_Minimize(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MinimizeWindow(this);
+        }
+
+        private void CommandBinding_Executed_Maximize(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MaximizeWindow(this);
+        }
+
+        private void CommandBinding_Executed_Restore(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.RestoreWindow(this);
+        }
+
+        private void CommandBinding_Executed_Close(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.CloseWindow(this);
+        }
+
+        private void MainWindowStateChangeRaised(object sender, EventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                MainWindowBorder.BorderThickness = new Thickness(8);
+                RestoreButton.Visibility = Visibility.Visible;
+                MaximizeButton.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                MainWindowBorder.BorderThickness = new Thickness(0);
+                RestoreButton.Visibility = Visibility.Collapsed;
+                MaximizeButton.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
