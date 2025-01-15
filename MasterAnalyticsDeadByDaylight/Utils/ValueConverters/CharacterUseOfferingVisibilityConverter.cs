28b4c6938ac4f4d6e73ce4829c401ef08da690f5
Light Play <lightplay456@gmail.com>
2025-01-15 18:32:14 +0300

    Конвертер для отображения количество взятого подношение у выживших и киллеров. В зависимости от того, есть ли доступ к выбранному подношению у одной из ролей.

---
+++ MasterAnalyticsDeadByDaylight/Utils/ValueConverters/CharacterUseOfferingVisibilityConverter.cs
@@ -0,0 +1,40 @@
+﻿using MasterAnalyticsDeadByDaylight.MVVM.Model.MSSQL_DB;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MasterAnalyticsDeadByDaylight.Utils.ValueConverters
+{
+    class CharacterUseOfferingVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //return value is Offering currentOffering && parameter is string param && ((currentOffering.IdRole == 2 && param == "Killer") || (currentOffering.IdRole == 3 && param == "Survivor") || currentOffering.IdRole == 5) ? Visibility.Visible : Visibility.Collapsed;
+            if (value is Offering currentOffering && parameter is string param)
+            {
+                if (currentOffering.IdRole == 2 && param == "Killer")
+                {
+                    return Visibility.Visible;
+                }
+                if (currentOffering.IdRole == 3 && param == "Survivor")
+                {
+                    return Visibility.Visible;
+                }
+                if (currentOffering.IdRole == 5)
+                {
+                    return Visibility.Visible;
+                }
+                else
+                {
+                    return Visibility.Collapsed;
+                }
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
