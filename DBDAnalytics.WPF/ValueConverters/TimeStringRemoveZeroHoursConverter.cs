6bb09bc1c874c451358c1f24c2f24c494b72dae2
Light Play <lightplay456@gmail.com>
2025-03-13 16:11:36 +0300

    Обрезка последних символов у string.

---
+++ DBDAnalytics.WPF/ValueConverters/TimeStringRemoveZeroHoursConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace DBDAnalytics.WPF.ValueConverters
+{
+    class TimeStringRemoveZeroHoursConverter : IValueConverter
+    {
+        public int CharactersToRemove { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string timeString)
+            {
+                return timeString.Substring(CharactersToRemove);
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
