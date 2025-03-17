66ac732458f3ff640d46f4418223157d86d9b8b4
Light Play <lightplay456@gmail.com>
2025-03-17 10:36:29 +0300

    Регистрация Frame.

--- DBDAnalytics.WPF/Factories/WindowFactories/MainWindowFactory.cs
+++ DBDAnalytics.WPF/Factories/WindowFactories/MainWindowFactory.cs
@@ -3,22 +3,29 @@ using DBDAnalytics.WPF.Interfaces;
 using DBDAnalytics.WPF.ViewModels.WindowVM;
 using DBDAnalytics.WPF.Views.Windows;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace DBDAnalytics.WPF.Factories.WindowFactories
 {
-    internal class MainWindowFactory(Func<MainWindowVM> viewModelFactory) : IWindowFactory
+    internal class MainWindowFactory(Func<MainWindowVM> viewModelFactory, IPageNavigationService pageNavigationService) : IWindowFactory
     {
         private readonly Func<MainWindowVM> _viewModelFactory = viewModelFactory;
+        private readonly IPageNavigationService _pageNavigationService = pageNavigationService;
 
         public Window CreateWindow(object parameter = null, TypeParameter typeParameter = TypeParameter.None)
         {
             var viewModel = _viewModelFactory();
             viewModel.Update(parameter, typeParameter);
+            var window = new MainWindow { DataContext = viewModel };
 
-            return new MainWindow
+            window.Loaded += (sender, args) =>
             {
-                DataContext = viewModel,
+                var mainFrame = window.FindName("MainFrame") as Frame;
+                _pageNavigationService.RegisterFrame(FrameName.MainFrame, mainFrame);
+                _pageNavigationService.Navigate(PageName.DashBoard, FrameName.MainFrame);
             };
+
+            return window;
         }
     }
 }
