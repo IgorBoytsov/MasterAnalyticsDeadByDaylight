700b2166541c280cddc8111eb53aea04de14a2e3
Light Play <lightplay456@gmail.com>
2025-03-17 09:45:14 +0300

    Сервис навигации для страниц.

---
+++ DBDAnalytics.WPF/Interfaces/IPageNavigationService.cs
@@ -0,0 +1,13 @@
+﻿using DBDAnalytics.WPF.Enums;
+using System.Windows.Controls;
+
+namespace DBDAnalytics.WPF.Interfaces
+{
+    internal interface IPageNavigationService
+    {
+        void Close(PageName pageName, FrameName frameName);
+        void Navigate(PageName pageName, FrameName frameName, object parameter = null, TypeParameter typeParameter = TypeParameter.None);
+        void RegisterFrame(FrameName frameName, Frame frame);
+        void TransmittingValue(PageName pageName, FrameName frameName, object parameter = null, TypeParameter typeParameter = TypeParameter.None);
+    }
+}
--- DBDAnalytics.WPF/Services/PageNavigationService.cs
+++ DBDAnalytics.WPF/Services/PageNavigationService.cs
@@ -1,6 +1,85 @@
-﻿namespace DBDAnalytics.WPF.Services
+﻿using DBDAnalytics.WPF.Enums;
+using DBDAnalytics.WPF.Interfaces;
+using System.Windows.Controls;
+
+namespace DBDAnalytics.WPF.Services
 {
-    internal class PageNavigationService
+    internal class PageNavigationService : IPageNavigationService
     {
+        private Dictionary<FrameName, Frame> _frames = [];
+        private Dictionary<PageName, Page> _pages = [];
+        private readonly Dictionary<string, IPageFactory> _pagesFactories = [];
+
+        public PageNavigationService(IEnumerable<IPageFactory> pageFactories)
+        {
+            _pagesFactories = pageFactories.ToDictionary(f => f.GetType().Name.Replace("Factory", ""), f => f);
+        }
+
+        public void RegisterFrame(FrameName frameName, Frame frame) 
+        {
+            if (!_frames.TryAdd(frameName, frame))
+            {
+                throw new Exception($"Frame с именем '{frameName}' уже зарегистрирована.");
+            }
+        }
+
+        public void Navigate(PageName pageName, FrameName frameName, object parameter = null, TypeParameter typeParameter = TypeParameter.None)
+        {
+            if (_pages.TryGetValue(pageName, out var pageExist))
+            {
+                if (pageExist.DataContext is IUpdatable viewModel)
+                {
+                    if (_frames.TryGetValue(frameName, out var frame))
+                    {
+                        viewModel.Update(parameter, typeParameter);
+                        frame.Navigate(pageExist);
+                    }
+                    else throw new Exception($"Frame с именем '{frameName}' не зарегистрировано.");
+                }
+                else throw new Exception($"У ViewModel страницы'{pageName}' не реализован интерфейс IUpdatable.");
+            }
+            else
+            {
+                if (_pagesFactories.TryGetValue(pageName.ToString(), out var factory))
+                {
+                    if ( _frames.TryGetValue(frameName, out var frame))
+                    {
+                        var page = factory.CreatePage(parameter, typeParameter);
+                        _pages.TryAdd(pageName, page);
+                        frame.Navigate(page);
+                    }
+                    else throw new Exception($"Frame с именем '{frameName}' не зарегистрирован.");
+                }
+                else throw new Exception($"Factory у страницы с именем '{pageName}' не существует.");
+            }
+        }
+
+        public void TransmittingValue(PageName pageName, FrameName frameName, object parameter = null, TypeParameter typeParameter = TypeParameter.None)
+        {
+            if (_pages.TryGetValue(pageName, out var pageExist))
+            {
+                if (pageExist.DataContext is IUpdatable viewModel)
+                {
+                    if (_frames.TryGetValue(frameName, out var frame))
+                    {
+                        viewModel.Update(parameter, typeParameter);
+                        frame.Navigate(pageExist);
+                    }
+                    else throw new Exception($"Frame с именем '{frameName}' не зарегистрировано.");
+                }
+                else throw new Exception($"У ViewModel страницы '{pageName}' не реализован интерфейс IUpdatable.");
+            }
+            else throw new Exception("Такой страницы либо фрейма не существует.");
+        }
+
+        public void Close(PageName pageName, FrameName frameName)
+        {
+            if (_pages.ContainsKey(pageName) && _frames.TryGetValue(frameName, out var frameExist))
+            {
+                frameExist.Navigate(null);
+                _pages.Remove(pageName);
+            }
+            else throw new Exception("Такой страницы либо фрейма не существует.");
+        }
     }
 }
