fadd1aca15b32ec82dd2489ec4d394fc89173bdf
Light Play <lightplay456@gmail.com>
2025-03-20 16:55:01 +0300

    Интерфейс для фабрики объектов страниц.

---
+++ DBDAnalytics.WPF/Interfaces/IPageFactory.cs
@@ -0,0 +1,10 @@
+﻿using DBDAnalytics.WPF.Enums;
+using System.Windows.Controls;
+
+namespace DBDAnalytics.WPF.Interfaces
+{
+    internal interface IPageFactory
+    {
+        Page CreatePage(object? parameter = null, TypeParameter typeParameter = TypeParameter.None);
+    }
+}
