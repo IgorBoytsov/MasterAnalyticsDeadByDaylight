0ec63dfa4cd6ef087c6214c65ff0bc96aecb0558
Light Play <lightplay456@gmail.com>
2025-03-23 11:39:18 +0300

    Создана VM для страницы KillerDetails.

---
+++ DBDAnalytics.WPF/ViewModels/PageVM/KillerDetailsVM.cs
@@ -0,0 +1,17 @@
+﻿using DBDAnalytics.WPF.Enums;
+using DBDAnalytics.WPF.Interfaces;
+
+namespace DBDAnalytics.WPF.ViewModels.PageVM
+{
+    internal class KillerDetailsVM : BaseVM, IUpdatable
+    {
+        public KillerDetailsVM()
+        {
+        }
+
+        public void Update(object parameter, TypeParameter typeParameter = TypeParameter.None)
+        {
+            
+        }
+    }
+}
