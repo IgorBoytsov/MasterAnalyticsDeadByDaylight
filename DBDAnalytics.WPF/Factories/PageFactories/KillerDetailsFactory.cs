731c2450d36cd093592f30b1fe6f6f94810e7f10
Light Play <lightplay456@gmail.com>
2025-03-23 11:38:17 +0300

    Реализация фабрики для страницы KillerDetails.

---
+++ DBDAnalytics.WPF/Factories/PageFactories/KillerDetailsFactory.cs
@@ -0,0 +1,24 @@
+﻿using DBDAnalytics.WPF.Enums;
+using DBDAnalytics.WPF.Interfaces;
+using DBDAnalytics.WPF.ViewModels.PageVM;
+using DBDAnalytics.WPF.Views.Pages;
+using System.Windows.Controls;
+
+namespace DBDAnalytics.WPF.Factories.PageFactories
+{
+    internal class KillerDetailsFactory(Func<KillerDetailsVM> viewModel) : IPageFactory
+    {
+        private readonly Func<KillerDetailsVM> _viewModelFactory = viewModel;
+
+        public Page CreatePage(object parameter = null, TypeParameter typeParameter = TypeParameter.None)
+        {
+            var viewModel = _viewModelFactory();
+            viewModel.Update(parameter, typeParameter);
+
+            return new KillerDetails
+            {
+                DataContext = viewModel,
+            };
+        }
+    }
+}
