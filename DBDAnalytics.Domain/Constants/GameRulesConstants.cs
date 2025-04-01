f137db977be3d84ff6d3ab3c06447a2d16e749ba
Light Play <lightplay456@gmail.com>
2025-04-01 18:01:24 +0300

    Константы для игровых правил.

---
+++ DBDAnalytics.Domain/Constants/GameRulesConstants.cs
@@ -0,0 +1,20 @@
+﻿namespace DBDAnalytics.Domain.Constants
+{
+    public static class GameRulesConstants
+    {
+        public const int MaxKills = 4;
+        public const int MinKills = 0;
+
+        public const int MaxHooks = 12;
+        public const int MinHooks = 0;
+
+        public const int MaxRecentGenerators = 5;
+        public const int MinRecentGenerators = 0;
+        
+        public const int MaxPrestige = 100;
+        public const int MinPrestige = 0;
+        
+        public const int MaxScore = 40000 ;
+        public const int MinScore = 0;
+    }
+}
