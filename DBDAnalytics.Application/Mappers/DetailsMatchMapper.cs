99d04279e5ab51c21d3ab9168229a85d0cdc6d9a
Light Play <lightplay456@gmail.com>
2025-04-02 13:42:32 +0300

    Маппер для моделей детализации матчей.

---
+++ DBDAnalytics.Application/Mappers/DetailsMatchMapper.cs
@@ -0,0 +1,56 @@
+﻿using DBDAnalytics.Application.DTOs.DetailsDTOs;
+using DBDAnalytics.Domain.DomainModels.DetailsModels;
+
+namespace DBDAnalytics.Application.Mappers
+{
+    public static class DetailsMatchMapper
+    {
+        public static DetailsMatchDTO ToDTO(this DetailsMatchDomain domain)
+        {
+            return new DetailsMatchDTO()
+            {
+               KillerDTO = domain.Killer.ToDTO(),
+
+                IdGameStatistic = domain.IdGameStatistic,
+
+                CountKill = domain.CountKill,
+                CountHook = domain.CountHook,
+                RecentGenerator = domain.RecentGenerator,
+                DurationMatch = domain.DurationMatch,
+                Date = domain.Date,
+                
+                FirstSurvivorInfo = domain.FirstSurvivorInfo?.ToDTO(),
+                SecondSurvivorInfo = domain.SecondSurvivorInfo?.ToDTO(),
+                ThirdSurvivorInfo = domain.ThirdSurvivorInfo?.ToDTO(),
+                FourthSurvivorInfo = domain.FourthSurvivorInfo?.ToDTO(),
+            };
+        }
+
+        public static DetailsMatchSurvivorDTO ToDTO(this DetailsMatchSurvivorDomain domain)
+        {
+            return new DetailsMatchSurvivorDTO()
+            {
+                IdSurvivor = domain.IdSurvivor,
+                IdPlatform = domain.IdPlatform,
+                IdTypeDeath = domain.IdTypeDeath,
+                Anonymous = domain.Anonymous,
+                Bot = domain.Bot
+            };
+        }
+
+        public static DetailsMatchKillerDTO ToDTO(this DetailsMatchKillerDomain domain)
+        {
+            return new DetailsMatchKillerDTO()
+            {
+                IdKiller = domain.IdKiller,
+                FirstAddonID = domain.FirstAddonID,
+                SecondAddonID = domain.SecondAddonID,
+                FirstPerkID = domain.FirstPerkID,
+                SecondPerkID = domain.SecondPerkID,
+                ThirdPerkID = domain.ThirdPerkID,
+                FourthPerkID = domain.FourthPerkID,
+                Score = domain.Score,
+            };
+        }
+    }
+}
