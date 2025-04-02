f9e484770936d0f1b5880a77407cca4392f64f62
Light Play <lightplay456@gmail.com>
2025-04-02 10:28:26 +0300

    Отображение расчетов.

---
+++ DBDAnalytics.Application/ApplicationModels/CalculationModels/LabeledValue.cs
@@ -0,0 +1,20 @@
+﻿namespace DBDAnalytics.Application.ApplicationModels.CalculationModels
+{
+    public class LabeledValue
+    {
+        /// <summary>
+        /// Отображение название элемента. 
+        /// </summary>
+        public string Name { get; set; } = null!;
+
+        /// <summary>
+        /// Значение для конвертеров. Опционально. 
+        /// </summary>
+        public double? ConverterValue { get; set; }
+
+        /// <summary>
+        /// Значение для отображение и расчетов.
+        /// </summary>
+        public double Value { get; set; }
+    }
+}
