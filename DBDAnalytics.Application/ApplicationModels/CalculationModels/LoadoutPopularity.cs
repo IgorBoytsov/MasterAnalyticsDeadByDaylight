599c15d173ad5ae25eebb493ef616c4e4d87a4fd
Light Play <lightplay456@gmail.com>
2025-04-02 10:29:08 +0300

    Отображение популярности улучшений для предметов, киллеров и тд.

---
+++ DBDAnalytics.Application/ApplicationModels/CalculationModels/LoadoutPopularity.cs
@@ -0,0 +1,19 @@
+﻿namespace DBDAnalytics.Application.ApplicationModels.CalculationModels
+{
+    public class LoadoutPopularity
+    {
+        public byte[]? Image { get; set; }
+
+        public string? Name { get; set; }
+
+        public int CountMatchWithItemLoadout { get; set; }
+
+        public int TotalMatch { get; set; }
+
+        public double PickRate { get; set; }
+
+        public double WinRateAllMatch { get; set; }
+
+        public double WinRateWithItemLoadoutMatch { get; set; }
+    }
+}
