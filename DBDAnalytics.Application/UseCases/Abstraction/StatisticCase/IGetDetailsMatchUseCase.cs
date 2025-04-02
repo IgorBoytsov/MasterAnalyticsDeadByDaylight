6ac44d68ea05bbfd9821be25030b19f76b98f1f5
Light Play <lightplay456@gmail.com>
2025-04-02 13:43:55 +0300

    Получение списка детализированных матчей.

---
+++ DBDAnalytics.Application/UseCases/Abstraction/StatisticCase/IGetDetailsMatchUseCase.cs
@@ -0,0 +1,9 @@
+﻿using DBDAnalytics.Application.DTOs.DetailsDTOs;
+
+namespace DBDAnalytics.Application.UseCases.Abstraction.StatisticCase
+{
+    public interface IGetDetailsMatchUseCase
+    {
+        Task<(List<DetailsMatchDTO> KillerDetails, int TotalMatches)> GetDetailsMatch(int idKiller, int idAssociation);
+    }
+}
---
+++ DBDAnalytics.Application/UseCases/Realization/StatisticCase/GetDetailsMatchUseCase.cs
@@ -0,0 +1,21 @@
+﻿using DBDAnalytics.Application.DTOs.DetailsDTOs;
+using DBDAnalytics.Application.Mappers;
+using DBDAnalytics.Application.UseCases.Abstraction.StatisticCase;
+using DBDAnalytics.Domain.Interfaces.Repositories;
+
+namespace DBDAnalytics.Application.UseCases.Realization.StatisticCase
+{
+    public class GetDetailsMatchUseCase(IDetailsMatchRepository killStatsRepository) : IGetDetailsMatchUseCase
+    {
+        private readonly IDetailsMatchRepository _killStatsRepository = killStatsRepository;
+
+        public async Task<(List<DetailsMatchDTO> KillerDetails, int TotalMatches)> GetDetailsMatch(int idKiller, int idAssociation)
+        {
+            var (KillerDetails, TotalMatch) = await _killStatsRepository.GetDetailsMatch(idKiller, idAssociation);
+
+            var dto = KillerDetails.Select(x => x.ToDTO());
+
+            return (dto.ToList(), TotalMatch);
+        }
+    }
+}
