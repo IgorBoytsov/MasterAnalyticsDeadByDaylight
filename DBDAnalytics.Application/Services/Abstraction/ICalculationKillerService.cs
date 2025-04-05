36b5c20fa09f3c5e7d5683598d28007277040030
Light Play <lightplay456@gmail.com>
2025-04-05 14:00:21 +0300

    Сервис для расчета данных по убийцам.

---
+++ DBDAnalytics.Application/Services/Abstraction/ICalculationKillerService.cs
@@ -0,0 +1,11 @@
+﻿using DBDAnalytics.Application.ApplicationModels.CalculationModels;
+using DBDAnalytics.Application.DTOs.DetailsDTOs;
+
+namespace DBDAnalytics.Application.Services.Abstraction
+{
+    public interface ICalculationKillerService
+    {
+        List<LabeledValue> DetailingHooks(List<DetailsMatchDTO> matches, string displayNameFormat);
+        List<LabeledValue> DetailingKills(List<DetailsMatchDTO> matches, string displayNameFormat);
+    }
+}
---
+++ DBDAnalytics.Application/Services/Realization/CalculationKillerService.cs
@@ -0,0 +1,19 @@
+﻿using DBDAnalytics.Application.ApplicationModels.CalculationModels;
+using DBDAnalytics.Application.DTOs;
+using DBDAnalytics.Application.DTOs.DetailsDTOs;
+using DBDAnalytics.Application.Services.Abstraction;
+using DBDAnalytics.Domain.Constants;
+
+namespace DBDAnalytics.Application.Services.Realization
+{
+    public class CalculationKillerService(ICreatingApplicationModelsService creatingApplicationModelsService) : ICalculationKillerService
+    {
+        private readonly ICreatingApplicationModelsService _creatingApplicationModelsService = creatingApplicationModelsService;
+
+        public List<LabeledValue> DetailingKills(List<DetailsMatchDTO> matches, string displayNameFormat) => 
+            _creatingApplicationModelsService.GenerateLabeledValues(matches, GameRulesConstants.MaxKills, i => matches.Count(x => x.CountKill == i), displayNameFormat);
+
+        public List<LabeledValue> DetailingHooks(List<DetailsMatchDTO> matches, string displayNameFormat) => 
+            _creatingApplicationModelsService.GenerateLabeledValues(matches, GameRulesConstants.MaxHooks, i => matches.Count(x => x.CountHook == i), displayNameFormat);
+    }
+}
