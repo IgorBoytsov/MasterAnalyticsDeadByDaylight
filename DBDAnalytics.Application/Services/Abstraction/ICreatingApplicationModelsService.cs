528946dd82d6f3c7ef9b63ddf62aed3b4ed5f67c
Light Play <lightplay456@gmail.com>
2025-04-05 13:58:27 +0300

    Сервис для создание моделей.

---
+++ DBDAnalytics.Application/Services/Abstraction/ICreatingApplicationModelsService.cs
@@ -0,0 +1,18 @@
+﻿using DBDAnalytics.Application.ApplicationModels.CalculationModels;
+using DBDAnalytics.Application.DTOs.DetailsDTOs;
+
+namespace DBDAnalytics.Application.Services.Abstraction
+{
+    public interface ICreatingApplicationModelsService
+    {
+        List<LabeledValue> GenerateLabeledValues(List<DetailsMatchDTO> matches, int maxValue, Func<int, int> countMatchesWithValue, string displayNameFormat);
+        LoadoutPopularity CreatedLoadoutPopularity(
+            string name,
+            byte[]? image,
+            int totalMatch,
+            int countMatchWithItemLoadout,
+            double pickRate,
+            double winRateAllMatch,
+            double winRateWithItemLoadoutMatch);
+    }
+}
---
+++ DBDAnalytics.Application/Services/Realization/CreatingApplicationModelsService.cs
@@ -0,0 +1,57 @@
+﻿using DBDAnalytics.Application.ApplicationModels.CalculationModels;
+using DBDAnalytics.Application.DTOs.DetailsDTOs;
+using DBDAnalytics.Application.Services.Abstraction;
+
+namespace DBDAnalytics.Application.Services.Realization
+{
+    public class CreatingApplicationModelsService(Lazy<ICalculationGeneralService> calculatorGeneralService) : ICreatingApplicationModelsService
+    {
+        private readonly Lazy<ICalculationGeneralService> _calculationGeneralService = calculatorGeneralService;
+
+        public List<LabeledValue> GenerateLabeledValues(
+            List<DetailsMatchDTO> matches,
+            int maxValue,
+            Func<int, int> countMatchesWithValue,
+            string displayNameFormat)
+        {
+            return Enumerable.Range(0, maxValue + 1)
+                .Select(i =>
+                {
+                    int count = countMatchesWithValue(i);
+                    return CreateLabeledValue(i, count, matches.Count, displayNameFormat);
+                })
+                .ToList();
+        }
+
+        private LabeledValue CreateLabeledValue(int i, int count, int totalMatches, string displayNameFormat)
+        {
+            return new LabeledValue
+            {
+                Name = string.Format(displayNameFormat, i),
+                ConverterValue = i,
+                Value = _calculationGeneralService.Value.Percentage(count, totalMatches)
+            };
+        }
+
+        public LoadoutPopularity CreatedLoadoutPopularity(
+            string name,
+            byte[]? image,
+            int totalMatch,
+            int countMatchWithItemLoadout,
+            double pickRate,
+            double winRateAllMatch,
+            double winRateWithItemLoadoutMatch)
+        {
+            return new LoadoutPopularity
+            {
+                Name = name,
+                Image = image,
+                TotalMatch = totalMatch,
+                CountMatchWithItemLoadout = countMatchWithItemLoadout,
+                PickRate = pickRate,
+                WinRateAllMatch = winRateAllMatch,
+                WinRateWithItemLoadoutMatch = winRateWithItemLoadoutMatch
+            };
+        }
+    }
+}
