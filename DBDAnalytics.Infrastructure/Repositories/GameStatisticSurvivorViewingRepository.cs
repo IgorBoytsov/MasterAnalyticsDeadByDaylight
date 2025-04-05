fa0fb2d26875e337dd050ccb004e782591f90a13
Light Play <lightplay456@gmail.com>
2025-04-05 14:41:16 +0300

    Исправлена ошибка, при которой EF core не мог преобразовать LINQ-запрос в SQL-запрос для поиска матча по id и его преобразование в GameStatisticSurvivorViewing.

--- DBDAnalytics.Infrastructure/Repositories/GameStatisticSurvivorViewingRepository.cs
+++ DBDAnalytics.Infrastructure/Repositories/GameStatisticSurvivorViewingRepository.cs
@@ -13,42 +13,75 @@ namespace DBDAnalytics.Infrastructure.Repositories
         {
             using (var _context = _contextFactory())
             {
-                var matches = await _context.GameStatistics
+                var rawData = await _context.GameStatistics
                     .Include(x => x.IdSurvivors1Navigation).ThenInclude(x => x.IdSurvivorNavigation)
                     .Include(x => x.IdSurvivors2Navigation).ThenInclude(x => x.IdSurvivorNavigation)
                     .Include(x => x.IdSurvivors3Navigation).ThenInclude(x => x.IdSurvivorNavigation)
                     .Include(x => x.IdSurvivors4Navigation).ThenInclude(x => x.IdSurvivorNavigation)
                     .Include(x => x.IdMapNavigation)
-                    .OrderByDescending(x => x.DateTimeMatch)
-                    .Where(x => x.IdGameStatistic == idGameStatistic)
-                    .Select(x => GameStatisticSurvivorViewingDomain.Create(
-                        x.IdGameStatistic,
+                        .Where(x => x.IdGameStatistic == idGameStatistic)
+                            .Select(x => new {
+                                x.IdGameStatistic,
 
-                        (x.IdSurvivors1Navigation.IdAssociation == 1 ? x.IdSurvivors1 :
-                         x.IdSurvivors2Navigation.IdAssociation == 1 ? x.IdSurvivors2 :
-                         x.IdSurvivors3Navigation.IdAssociation == 1 ? x.IdSurvivors3 :
-                         x.IdSurvivors4Navigation.IdAssociation == 1 ? x.IdSurvivors4 : 0),
+                                x.IdSurvivors1,
+                                Survivor1_IdAssociation = x.IdSurvivors1Navigation.IdAssociation,
+                                Survivor1_IdTypeDeath = x.IdSurvivors1Navigation.IdTypeDeath,
+                                Survivor1_Image = x.IdSurvivors1Navigation.IdSurvivorNavigation.SurvivorImage,
 
-                        (x.IdSurvivors1Navigation.IdAssociation == 1 ? x.IdSurvivors1Navigation.IdTypeDeath :
-                         x.IdSurvivors2Navigation.IdAssociation == 1 ? x.IdSurvivors1Navigation.IdTypeDeath :
-                         x.IdSurvivors3Navigation.IdAssociation == 1 ? x.IdSurvivors1Navigation.IdTypeDeath :
-                         x.IdSurvivors4Navigation.IdAssociation == 1 ? x.IdSurvivors1Navigation.IdTypeDeath : 0),
+                                x.IdSurvivors2,
+                                Survivor2_IdAssociation = x.IdSurvivors2Navigation.IdAssociation,
+                                Survivor2_IdTypeDeath = x.IdSurvivors2Navigation.IdTypeDeath,
+                                Survivor2_Image = x.IdSurvivors2Navigation.IdSurvivorNavigation.SurvivorImage,
+                                                                                                       
+                                x.IdSurvivors3,
+                                Survivor3_IdAssociation = x.IdSurvivors3Navigation.IdAssociation,
+                                Survivor3_IdTypeDeath = x.IdSurvivors3Navigation.IdTypeDeath,
+                                Survivor3_Image = x.IdSurvivors3Navigation.IdSurvivorNavigation.SurvivorImage,
+                                                                                                       
+                                x.IdSurvivors4,
+                                Survivor4_IdAssociation = x.IdSurvivors4Navigation.IdAssociation,
+                                Survivor4_IdTypeDeath = x.IdSurvivors4Navigation.IdTypeDeath,
+                                Survivor4_Image = x.IdSurvivors4Navigation.IdSurvivorNavigation.SurvivorImage,
+                                                                                                      
+                                x.DateTimeMatch,
+                                x.GameTimeMatch,
+                                x.IdMapNavigation.MapName,
+                                x.CountKills,
+                                x.CountHooks,
+                                x.NumberRecentGenerators
+                            })
+                            .FirstOrDefaultAsync(); 
 
-                        (x.IdSurvivors1Navigation.IdAssociation == 1 ? x.IdSurvivors1Navigation.IdSurvivorNavigation.SurvivorImage :
-                         x.IdSurvivors2Navigation.IdAssociation == 1 ? x.IdSurvivors2Navigation.IdSurvivorNavigation.SurvivorImage :
-                         x.IdSurvivors3Navigation.IdAssociation == 1 ? x.IdSurvivors3Navigation.IdSurvivorNavigation.SurvivorImage :
-                         x.IdSurvivors4Navigation.IdAssociation == 1 ? x.IdSurvivors4Navigation.IdSurvivorNavigation.SurvivorImage : null),
+                if (rawData == null)
+                    return null;
 
-                        x.DateTimeMatch,
-                        x.GameTimeMatch,
-                        x.IdMapNavigation.MapName,
-                        x.CountKills,
-                        x.CountHooks,
-                        x.NumberRecentGenerators)
-                    .GameStatisticSurvivorViewingDomain)
-                    .FirstOrDefaultAsync(x => x.IdGameStatistic == idGameStatistic);
+                var result = GameStatisticSurvivorViewingDomain.Create(
+                    rawData.IdGameStatistic,
 
-                return matches;
+                    (rawData.Survivor1_IdAssociation == 1 ? rawData.IdSurvivors1 :
+                     rawData.Survivor2_IdAssociation == 1 ? rawData.IdSurvivors2 :
+                     rawData.Survivor3_IdAssociation == 1 ? rawData.IdSurvivors3 :
+                     rawData.Survivor4_IdAssociation == 1 ? rawData.IdSurvivors4 : 0),
+
+                    (rawData.Survivor1_IdAssociation == 1 ? rawData.Survivor1_IdTypeDeath :
+                     rawData.Survivor2_IdAssociation == 1 ? rawData.Survivor2_IdTypeDeath :
+                     rawData.Survivor3_IdAssociation == 1 ? rawData.Survivor3_IdTypeDeath :
+                     rawData.Survivor4_IdAssociation == 1 ? rawData.Survivor4_IdTypeDeath : 0),
+
+                    (rawData.Survivor1_IdAssociation == 1 ? rawData.Survivor1_Image :
+                     rawData.Survivor2_IdAssociation == 1 ? rawData.Survivor2_Image :
+                     rawData.Survivor3_IdAssociation == 1 ? rawData.Survivor3_Image :
+                     rawData.Survivor4_IdAssociation == 1 ? rawData.Survivor4_Image : null), 
+
+                    rawData.DateTimeMatch,
+                    rawData.GameTimeMatch,
+                    rawData.MapName,
+                    rawData.CountKills,
+                    rawData.CountHooks,
+                    rawData.NumberRecentGenerators
+                );
+
+                return result.GameStatisticSurvivorViewingDomain;
             }
         }
 
