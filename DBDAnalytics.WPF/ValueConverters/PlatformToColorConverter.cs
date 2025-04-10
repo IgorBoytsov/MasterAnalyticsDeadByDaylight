73c03e12042cfbacb71cd96182d597a153378b41
Light Play <lightplay456@gmail.com>
2025-04-10 11:19:53 +0300

    Конвертер для отображения цвета по игровым платформам.

---
+++ DBDAnalytics.WPF/ValueConverters/PlatformToColorConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace DBDAnalytics.WPF.ValueConverters
+{
+    internal class PlatformToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string platformName)
+            {
+                return platformName switch
+                {
+                    "Steam" => System.Windows.Application.Current.Resources["Brush.Steam"] as SolidColorBrush,
+                    "Кроссплатформа" => System.Windows.Application.Current.Resources["Brush.Crosplatform"] as SolidColorBrush,
+                    _ => () => Brushes.Transparent
+                };
+            }
+            return Brushes.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
