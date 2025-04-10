d6a766f8cb40df4fe2819a97f433f05af43f734a
Light Play <lightplay456@gmail.com>
2025-04-10 11:17:16 +0300

    Конвертер для отображение визуальной статистики по выходу игроков из матча.

---
+++ DBDAnalytics.WPF/ValueConverters/BotToColorConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace DBDAnalytics.WPF.ValueConverters
+{
+    internal class BotToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string platformName)
+            {
+                return platformName switch
+                {
+                    "Отключились: " => System.Windows.Application.Current.Resources["Brush.Bot"] as SolidColorBrush,
+                    "Не отключились: " => System.Windows.Application.Current.Resources["Brush.NoBot"] as SolidColorBrush,
+                    _ => () => Brushes.Transparent
+                };
+            }
+            return Brushes.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
