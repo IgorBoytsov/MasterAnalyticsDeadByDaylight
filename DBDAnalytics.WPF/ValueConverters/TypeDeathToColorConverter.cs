1a1c3a5ca1d396dc4d614e5b8da911a78f31c22a
Light Play <lightplay456@gmail.com>
2025-04-10 11:32:06 +0300

    Конвертер для отображения цвета по типам смерти выживщих.

---
+++ DBDAnalytics.WPF/ValueConverters/TypeDeathToColorConverter.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace DBDAnalytics.WPF.ValueConverters
+{
+    internal class TypeDeathToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string platformName)
+            {
+                return platformName switch
+                {
+                    "От крюка" => System.Windows.Application.Current.Resources["Brush.DeathByHook"] as SolidColorBrush,
+                    "От земли" => System.Windows.Application.Current.Resources["Brush.DeathByGround"] as LinearGradientBrush,
+                    "От мементо" => System.Windows.Application.Current.Resources["Brush.DeathByMementoMori"] as SolidColorBrush,
+                    "От способности убийцы" => System.Windows.Application.Current.Resources["Brush.DeathByKillerAbility"] as LinearGradientBrush,
+                    "Сбежал" => System.Windows.Application.Current.Resources["Brush.DeathByEscape"] as SolidColorBrush,
+                    _ => () => Brushes.Transparent
+                };
+            }
+            return Brushes.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
