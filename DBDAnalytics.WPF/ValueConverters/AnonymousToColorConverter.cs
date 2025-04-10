decda15d3cd97992342df8c4d5ce4c3ab2e00171
Light Play <lightplay456@gmail.com>
2025-04-10 11:32:29 +0300

    Конвертер для отображения цвета по уровню анонимности игроков.

---
+++ DBDAnalytics.WPF/ValueConverters/AnonymousToColorConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace DBDAnalytics.WPF.ValueConverters
+{
+    internal class AnonymousToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string platformName)
+            {
+                return platformName switch
+                {
+                    "Анонимных: " => System.Windows.Application.Current.Resources["Brush.Anonymous"] as SolidColorBrush,
+                    "Не анонимных: " => System.Windows.Application.Current.Resources["Brush.NoAnonymous"] as SolidColorBrush,
+                    _ => () => Brushes.Transparent
+                };
+            }
+            return Brushes.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
