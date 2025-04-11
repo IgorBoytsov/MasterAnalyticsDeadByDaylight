ff3225721c570a0c8b0e0f330457bbc5ab584fba
Light Play <lightplay456@gmail.com>
2025-04-11 11:07:58 +0300

    Преобразует число в строку вида : разделенного числа на группы разрядов.

---
+++ DBDAnalytics.WPF/ValueConverters/NumberDigitConverter.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace DBDAnalytics.WPF.ValueConverters
+{
+    internal class NumberDigitConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double number)
+            {
+                CultureInfo customCulture = (CultureInfo)CultureInfo.InvariantCulture.Clone();
+                customCulture.NumberFormat.NumberGroupSeparator = " "; 
+
+                string formatString = "#,##0.################";
+
+                string formattedNumber = number.ToString(formatString, customCulture);
+
+                if (formattedNumber == "-0")
+                {
+                    return "0";
+                }
+
+                return formattedNumber;
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
