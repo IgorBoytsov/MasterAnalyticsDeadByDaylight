4788d912c820ed4a1fad9865b5cc5f0a6963aff1
Light Play <lightplay456@gmail.com>
2025-04-11 17:34:55 +0300

    UseCase для получение информации о матче.

---
+++ DBDAnalytics.Application/UseCases/Abstraction/GameStatisticCase/IGetDetailsMatchViewUseCase.cs
@@ -0,0 +1,9 @@
+﻿using DBDAnalytics.Application.DTOs.DetailsViewDTOs;
+
+namespace DBDAnalytics.Application.UseCases.Abstraction.GameStatisticCase
+{
+    public interface IGetDetailsMatchViewUseCase
+    {
+        Task<DetailsMatchViewDTO> GetDetailsViewMatch(int idGameStatistics);
+    }
+}
---
+++ DBDAnalytics.Application/UseCases/Realization/GameStatisticCase/GetDetailsMatchViewUseCase.cs
@@ -0,0 +1,20 @@
+﻿using DBDAnalytics.Application.DTOs.DetailsViewDTOs;
+using DBDAnalytics.Application.Mappers;
+using DBDAnalytics.Application.UseCases.Abstraction.GameStatisticCase;
+using DBDAnalytics.Domain.Interfaces.Repositories;
+
+namespace DBDAnalytics.Application.UseCases.Realization.GameStatisticCase
+{
+    public class GetDetailsMatchViewUseCase(IDetailsMatchRepository detailsMatchRepository) : IGetDetailsMatchViewUseCase
+    {
+        private readonly IDetailsMatchRepository _detailsMatchRepository = detailsMatchRepository;
+        public async Task<DetailsMatchViewDTO> GetDetailsViewMatch(int idGameStatistics)
+        {
+            var details = await _detailsMatchRepository.GetDetailsViewMatch(idGameStatistics);
+
+            var dto = details.ToDTO();
+
+            return dto;
+        }
+    }
+}
