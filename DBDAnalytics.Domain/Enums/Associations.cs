f46ec01b0a5692109b99951a6ecc1170e9a12f84
Light Play <lightplay456@gmail.com>
2025-04-14 17:07:48 +0300

    Перечисление игровых ассоциаций.

---
+++ DBDAnalytics.Domain/Enums/Associations.cs
@@ -0,0 +1,11 @@
+﻿namespace DBDAnalytics.Domain.Enums
+{
+    public enum Associations
+    {
+        None = 0,  
+        Me = 1,
+        Friend = 2,
+        Opponent = 3,
+        RandomPlayer = 4,
+    }
+}
