8424966c45d9dec08141b3c540e3a2c9f3d392dd
Light Play <lightplay456@gmail.com>
2025-04-21 12:02:21 +0300

    Перечисление ID типов смертей у выживших.

---
+++ DBDAnalytics.Domain/Enums/TypeDeaths.cs
@@ -0,0 +1,11 @@
+﻿namespace DBDAnalytics.Domain.Enums
+{
+    public enum TypeDeaths
+    {
+        FromHook = 1,
+        FromGround = 2,
+        FromMemento = 3,
+        FromKillerAbility = 4,
+        Escape = 5,
+    }
+}
