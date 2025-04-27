c962a8822a97be8cba332e80dd7ae09641f08f94
Light Play <lightplay456@gmail.com>
2025-04-27 11:52:15 +0300

    Добавлен сервис для расчета по игровым предметам.

---
+++ DBDAnalytics.Application/Services/Abstraction/ICalculationItemService.cs
@@ -0,0 +1,10 @@
+﻿using DBDAnalytics.Application.DTOs.DetailsDTOs;
+using DBDAnalytics.Domain.Enums;
+
+namespace DBDAnalytics.Application.Services.Abstraction
+{
+    public interface ICalculationItemService
+    {
+        List<DetailsMatchSurvivorDTO> GetSurvivors(List<DetailsMatchDTO> matches, int idItem, Associations association);
+    }
+}
---
+++ DBDAnalytics.Application/Services/Realization/CalculationItemService.cs
@@ -0,0 +1,28 @@
+﻿using DBDAnalytics.Application.DTOs.DetailsDTOs;
+using DBDAnalytics.Application.Services.Abstraction;
+using DBDAnalytics.Domain.Enums;
+
+namespace DBDAnalytics.Application.Services.Realization
+{
+    public class CalculationItemService : ICalculationItemService
+    {
+        public List<DetailsMatchSurvivorDTO> GetSurvivors(List<DetailsMatchDTO> matches, int idItem, Associations association)
+        {
+            if (association == Associations.None || matches == null)
+                return null;
+
+            int associationId = (int)association;
+
+            return matches.SelectMany(match =>
+            {
+                List<DetailsMatchSurvivorDTO> survivors = [];
+
+                if (match.FirstSurvivorInfo?.IdAssociation == associationId && match.FirstSurvivorInfo.IdItem == idItem) survivors.Add(match.FirstSurvivorInfo);
+                if (match.SecondSurvivorInfo?.IdAssociation == associationId && match.SecondSurvivorInfo.IdItem == idItem) survivors.Add(match.SecondSurvivorInfo);
+                if (match.ThirdSurvivorInfo?.IdAssociation == associationId && match.ThirdSurvivorInfo.IdItem == idItem) survivors.Add(match.ThirdSurvivorInfo);
+                if (match.FourthSurvivorInfo?.IdAssociation == associationId && match.FourthSurvivorInfo.IdItem == idItem) survivors.Add(match.FourthSurvivorInfo);
+                return survivors;
+            }).ToList();
+        }
+    }
+}
