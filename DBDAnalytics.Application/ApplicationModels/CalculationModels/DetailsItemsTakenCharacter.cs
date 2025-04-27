46b5d27e1480ed3fdd242b4d826bbfc1663d1994
Light Play <lightplay456@gmail.com>
2025-04-27 11:54:24 +0300

    Класс для отображение сколько предметов взял конкретный персонаж, с небольшой статистикой.

---
+++ DBDAnalytics.Application/ApplicationModels/CalculationModels/DetailsItemsTakenCharacter.cs
@@ -0,0 +1,15 @@
+﻿namespace DBDAnalytics.Application.ApplicationModels.CalculationModels
+{
+    public class DetailsItemsTakenCharacter
+    {
+        public int CharacterId { get; set; }
+
+        public byte[]? CharacterImage { get; set; }
+
+        public string CharacterName { get; set; } = null!;
+
+        public int CountPick { get; set; }
+
+        public double PickRate { get; set; }
+    }
+}
