14495ab5f21c3bc92cb071aa02892fde448c7987
Light Play <lightplay456@gmail.com>
2025-09-16 13:07:10 +0300

    feat(Shared-WPF): добавить PopupController для управления всплывающими окнами
    Реализован PopupController, который действует как ViewModel для элемента WPF - Popup. Он позволяет инкапсулировать логику управления состоянием всплывающего окна.
    
    Это упрощает взаимодействие с Popup из основной ViewModel, следуя принципам MVVM и избегая необходимости в code-behind.

---
+++ v3/src/shared/ui/Shared.WPF/ViewModels/Components/IPopupController.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Shared.WPF.ViewModels.Components
+{
+    public interface IPopupController : INotifyPropertyChanged
+    {
+        bool IsOpen { get; set; }
+        bool StaysOpen { get; set; }
+        UIElement? PlacementTarget { get; set; }
+
+        ICommand ShowCommand { get; }
+        ICommand HideCommand { get; }
+    }
+}
---
+++ v3/src/shared/ui/Shared.WPF/ViewModels/Components/PopupController.cs
@@ -0,0 +1,55 @@
+﻿using Shared.WPF.Commands;
+using Shared.WPF.ViewModels.Base;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Shared.WPF.ViewModels.Components
+{
+    public class PopupController : BaseViewModel, IPopupController 
+    {
+        private bool _isOpen;
+        public bool IsOpen
+        {
+            get => _isOpen;
+            set => SetProperty(ref _isOpen, value);
+        }
+
+        private bool _staysOpen = false;
+        public bool StaysOpen
+        {
+            get => _staysOpen;
+            set => SetProperty(ref _staysOpen, value);
+        }
+
+        private UIElement? _placementTarget;
+        public UIElement? PlacementTarget
+        {
+            get => _placementTarget;
+            set => SetProperty(ref _placementTarget, value);
+        }
+
+        public ICommand ShowCommand { get; private set; }
+        public ICommand HideCommand { get; private set; }
+
+        public PopupController()
+        {
+            ShowCommand = new RelayCommand<object>(Execute_Show);
+            HideCommand = new RelayCommand(Execute_Hide);
+        }
+
+        private void Execute_Show(object parameter)
+        {
+            if (parameter is UIElement target)
+            {
+                PlacementTarget = target;
+                IsOpen = true;
+            }
+        }
+
+        private void Execute_Hide()
+        {
+            IsOpen = false;
+            PlacementTarget = null;
+        }
+    }
+}
