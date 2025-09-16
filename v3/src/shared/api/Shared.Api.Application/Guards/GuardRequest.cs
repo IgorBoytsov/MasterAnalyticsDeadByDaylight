a3c06e0b32131c4101876aad5b05e0372ce189a3
Light Play <lightplay456@gmail.com>
2025-09-16 13:33:46 +0300

    refactor(structure ): переместить общие API проекты в единую папку
    Проекты Shared.Api, Shared.Api.Application и Shared.Api.Infrastructure находились во вложенной папке services/Shared. Это создавало путаницу и дублирование, так как на верхнем уровне уже существует основная папка shared для переиспользуемого кода.
    
    Для унификации и улучшения архитектуры проекта, эти библиотеки были перенесены в новую, более логичную директорию shared/api.
    
    Это изменение решает следующие задачи:
    - Устраняет дублирование "общих" папок.
    - Группирует весь переиспользуемый код в одном месте.
    - Делает структуру проекта более понятной и предсказуемой.

---
+++ v3/src/shared/api/Shared.Api.Application/Guards/GuardRequest.cs
@@ -0,0 +1,36 @@
+﻿using DBDAnalytics.Shared.Domain.Results;
+
+namespace Shared.Api.Application.Guards
+{
+    public static class GuardRequest
+    {
+        public static bool TryGetFailureResult<TRequest>(TRequest? request, out Result failureResult)
+        {
+            if (request is not null)
+            {
+                failureResult = null!;
+                return false;
+            }
+
+            var systemMessage = $"Запрос типа '{typeof(TRequest).Name}' не может быть null.";
+            var clientMessage = "Ошибка валидации на стороне сервера. Пожалуйста повторите попытку позже.";
+
+            failureResult = Result.Failure(new Error(ErrorCode.Null, clientMessage));
+            return true;
+        }
+
+        public static bool TryGetFailureResult<TRequest, TResult>(TRequest? request, out Result<TResult> failureResult)
+        {
+            if (request is not null)
+            {
+                failureResult = null!;
+                return false;
+            }
+            var systemMessage = $"Запрос типа '{typeof(TRequest).Name}' не может быть null.";
+            var clientMessage = "Ошибка валидации на стороне сервера. Пожалуйста повторите попытку позже.";
+
+            failureResult = Result<TResult>.Failure(new Error(ErrorCode.Null, clientMessage));
+            return true;
+        }
+    }
+}
