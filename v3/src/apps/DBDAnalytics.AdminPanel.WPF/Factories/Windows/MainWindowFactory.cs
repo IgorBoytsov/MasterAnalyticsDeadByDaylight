3f0c50ec084daafe9b3a5387a3612c17c7943dab
Light Play <lightplay456@gmail.com>
2025-09-17 18:31:43 +0300

    feat(admin-panel-wpf): реализовать базовую структуру для MainWindow
    Добавлены View, ViewModel и фабрика для главного окна, что создает основу для дальнейшей разработки интерфейса приложения.
    
    Ключевые изменения:
    - MainWindow.xaml / .cs: Представление главного окна.
    - MainWindowViewModel.cs: ViewModel, которая будет содержать логику и состояние для главного окна.
    - MainWindowFactory.cs: Фабрика для инкапсуляции логики создания экземпляра MainWindow с его зависимостями.
    
    Этот подход соответствует паттерну MVVM и принципам чистого кода, отделяя представление от логики.

---
+++ v3/src/apps/DBDAnalytics.AdminPanel.WPF/Factories/Windows/MainWindowFactory.cs
@@ -0,0 +1,34 @@
+﻿using DBDAnalytics.AdminPanel.WPF.ViewModels.Windows;
+using DBDAnalytics.AdminPanel.WPF.Views.Windows;
+using Shared.WPF.Enums;
+using Shared.WPF.Navigations.Pages;
+using Shared.WPF.Navigations.Windows;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DBDAnalytics.AdminPanel.WPF.Factories.Windows
+{
+    internal sealed class MainWindowFactory(
+        Func<MainWindowViewModel> vmFactory, 
+        IPageNavigation pageNavigation) : IWindowFactory
+    {
+        private readonly Func<MainWindowViewModel> _vmFactory = vmFactory;
+        private readonly IPageNavigation _pageNavigation = pageNavigation;
+
+        public Window CreateWindow()
+        {
+            var vm = _vmFactory();
+
+            var window = new MainWindow { DataContext = vm };
+
+            window.Loaded += (sender, args) =>
+            {
+                var mainFrame = window.FindName("MainFrame") as Frame; 
+                _pageNavigation.RegisterFrame(FramesName.MainFrame, mainFrame);
+                //_pageNavigation.Navigate(PagesName.GameMode, FramesName.MainFrame);
+            };
+
+            return window;
+        }
+    }
+}
---
+++ v3/src/apps/DBDAnalytics.AdminPanel.WPF/ViewModels/Windows/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Shared.WPF.Navigations.Pages;
+using Shared.WPF.Navigations.Windows;
+using Shared.WPF.ViewModels.Base;
+
+namespace DBDAnalytics.AdminPanel.WPF.ViewModels.Windows
+{
+    internal sealed class MainWindowViewModel(
+        IWindowNavigation windowNavigation,
+        IPageNavigation pageNavigation) : BaseWindowViewModel(windowNavigation, pageNavigation)
+    {
+        /*--Коллекции-------------------------------------------------------------------------------------*/
+
+        /*--Свойства--------------------------------------------------------------------------------------*/
+
+        /*--Команды---------------------------------------------------------------------------------------*/
+
+        /*--Методы----------------------------------------------------------------------------------------*/
+    }
+}
