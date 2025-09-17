76c4e82f3e9561587584cf3c58db273c35d709b6
Light Play <lightplay456@gmail.com>
2025-09-17 18:24:43 +0300

    feat(admin-panel-wpf): добавить WPF-приложение панели администратора

---
+++ v3/src/apps/DBDAnalytics.AdminPanel.WPF/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using DBDAnalytics.AdminPanel.WPF.Ioc;
+using DBDAnalytics.CatalogService.Client.Ioc;
+using DBDAnalytics.FIleStorageService.Client;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Shared.WPF.Enums;
+using Shared.WPF.Navigations.Pages;
+using Shared.WPF.Navigations.Windows;
+using Shared.WPF.Services;
+using System.Windows;
+
+namespace DBDAnalytics.AdminPanel.WPF
+{
+    public partial class App : Application
+    {
+        public IServiceProvider ServiceProvider { get; private set; } = null!;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var configurationBuilder = new ConfigurationBuilder()
+                     .SetBasePath(AppContext.BaseDirectory)
+                     .AddJsonFile("apiconfig.json", optional: false, reloadOnChange: true);
+
+            IConfigurationRoot configuration = configurationBuilder.Build();
+
+            var services = new ServiceCollection();
+
+            services.ConfigureCatalogApiHttpService(configuration);
+            services.ConfigureFileStorageApiHttpService(configuration);
+            services.ConfigurePage();
+            services.ConfigureWindows();
+
+            services.AddSingleton<IFileDialogService, FileDialogService>();
+
+            services.AddSingleton<IWindowNavigation, WindowNavigation>();
+            services.AddSingleton<IPageNavigation, PageNavigation>();
+
+            ServiceProvider = services.BuildServiceProvider();
+
+            ServiceProvider.GetService<IWindowNavigation>()!.Open(WindowsName.MainWindow);
+
+            base.OnStartup(e);
+        }
+    }
+}
