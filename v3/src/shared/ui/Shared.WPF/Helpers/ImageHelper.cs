2816898209c6ba1339bf1958180d44e6836751fc
Light Play <lightplay456@gmail.com>
2025-10-18 18:20:57 +0300

    feat(ImageHelper): добавить методы для обрезки и конвертации изображений
    Добавлены два новых статических метода в класс ImageHelper для расширения функциональности по обработке изображений.
    
    - CropImage: Позволяет обрезать изображение по заданным координатам и размерам из файла. Включает валидацию, чтобы область обрезки не выходила за пределы исходного изображения.
    
    - ToBitmapImageFromBitmap: Конвертирует объект System.Drawing.Bitmap в BitmapImage (WPF). Это полезно для отображения динамически созданных или обработанных Bitmap в интерфейсе WPF.

--- v3/src/shared/ui/Shared.WPF/Helpers/ImageHelper.cs
+++ v3/src/shared/ui/Shared.WPF/Helpers/ImageHelper.cs
@@ -1,6 +1,9 @@
 ﻿using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Net.Http;
+using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
@@ -95,6 +98,52 @@ namespace Shared.WPF.Helpers
             bitmapImage.Freeze();
 
             return bitmapImage;
+        }
+
+        public static BitmapImage ToBitmapImageFromBitmap(Bitmap bitmap)
+        {
+            try
+            {
+                using MemoryStream memory = new();
+
+                bitmap.Save(memory, ImageFormat.Png);
+
+                memory.Seek(0, SeekOrigin.Begin);
+
+                BitmapImage bitmapImage = new();
+
+                bitmapImage.BeginInit();
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.StreamSource = memory;
+                bitmapImage.EndInit();
+
+                bitmapImage.Freeze();
+
+                return bitmapImage;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Не получилось от рендерить изображение \n {ex.Message}");
+                return null;
+            }
+        }
+
+        public static Bitmap CropImage(string path, int x, int y, int width, int height)
+        {
+            using var image = new Bitmap(path);
+
+            if (x < 0 || y < 0 || x + width > image.Width || y + height > image.Height)
+            {
+                throw new Exception($"Координаты области обрезки находятся за пределами изображения.");
+            }
+
+            Bitmap croppedImage = new(width, height);
+
+            using (Graphics g = Graphics.FromImage(croppedImage))
+            {
+                g.DrawImage(image, 0, 0, new Rectangle(x, y, width, height), GraphicsUnit.Pixel);
+            }
+            return croppedImage;
         }
     }
 }
