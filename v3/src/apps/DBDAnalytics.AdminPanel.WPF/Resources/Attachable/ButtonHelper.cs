24dff538f0bb97465eead95f21542c66f9b86327
Light Play <lightplay456@gmail.com>
2025-10-26 18:06:26 +0300

    feat(ui): добавить хелпер ButtonHelper для иконок
    Реализован вспомогательный класс ButtonHelper с присоединенным свойством IconTemplate для установки шаблона иконки на кнопках.

---
+++ v3/src/apps/DBDAnalytics.AdminPanel.WPF/Resources/Attachable/ButtonHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DBDAnalytics.AdminPanel.WPF.Resources.Attachable
+{
+    public static class ButtonHelper
+    {
+        public static readonly DependencyProperty IconTemplateProperty =
+            DependencyProperty.RegisterAttached(
+                "IconTemplate",
+                typeof(ControlTemplate),
+                typeof(ButtonHelper),
+                new PropertyMetadata(null));
+
+        public static void SetIconTemplate(DependencyObject element, ControlTemplate value)
+        {
+            element.SetValue(IconTemplateProperty, value);
+        }
+
+        public static ControlTemplate GetIconTemplate(DependencyObject element)
+            => (ControlTemplate)element.GetValue(IconTemplateProperty);
+    }
+}
