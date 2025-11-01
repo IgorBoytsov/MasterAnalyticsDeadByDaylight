9a9523876ed753360a87380ff83346e2ebb32936
Light Play <lightplay456@gmail.com>
2025-11-01 19:15:47 +0300

    feat(client): добавить клиентскую библиотеку для MatchService
    Добавлена новая клиентская библиотека для взаимодействия с API сервиса матчей.

---
+++ v3/src/clients/DBDAnalytics.MatchService.Client/ApiClients/IMatchService.cs
@@ -0,0 +1,9 @@
+﻿using DBDAnalytics.Shared.Contracts.Responses.MatchService;
+using Shared.HttpClients.Abstractions;
+
+namespace DBDAnalytics.MatchService.Client.ApiClients
+{
+    public interface IMatchService : IBaseWriteApiService<MatchDetailResponse, string>, IBaseReadApiService<MatchDetailResponse, string>
+    {
+    }
+}
---
+++ v3/src/clients/DBDAnalytics.MatchService.Client/ApiClients/MatchApiService.cs
@@ -0,0 +1,10 @@
+﻿using DBDAnalytics.Shared.Contracts.Responses.MatchService;
+using Shared.HttpClients;
+
+namespace DBDAnalytics.MatchService.Client.ApiClients
+{
+    public sealed class MatchApiService(HttpClient client) 
+        : BaseApiService<MatchDetailResponse, string>(client, "api/matches"), IMatchService
+    {
+    }
+}
