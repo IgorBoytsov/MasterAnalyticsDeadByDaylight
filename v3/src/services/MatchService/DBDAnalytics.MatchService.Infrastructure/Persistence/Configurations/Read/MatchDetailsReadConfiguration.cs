bd624a28f3fb085766ed1451a8f9a5e8ddd4b584
Light Play <lightplay456@gmail.com>
2025-11-12 15:55:56 +0300

    refactor(styles): вынести общие стили в библиотеку Shared.Wpf
    Общие словари ресурсув были
    перенесены в Shared.Wpf для устранения
    дублирования кода и централизации UI-компонентов.
    
    Это упростит дальнейшую поддержку и обеспечит единый внешний
    вид для всех модулей приложения.

--- v3/src/services/MatchService/DBDAnalytics.MatchService.Infrastructure/Persistence/Configurations/Read/MatchDetailsReadConfiguration.cs
+++ v3/src/services/MatchService/DBDAnalytics.MatchService.Infrastructure/Persistence/Configurations/Read/MatchDetailsReadConfiguration.cs
@@ -8,34 +8,14 @@ namespace DBDAnalytics.MatchService.Infrastructure.Persistence.Configurations.Re
     {
         public void Configure(EntityTypeBuilder<MatchDetailsView> builder)
         {
-            //builder.ToView("V_MatchDetails");
-            //builder.HasKey(m => m.MatchId);
-
-            // 1. Указываем, что эта сущность сопоставлена с представлением (VIEW) и имеет первичный ключ.
             builder.ToView("v_match_details");
             builder.HasKey(m => m.MatchId);
 
-            // 2. Конфигурируем свойство 'KillerDetails'
-            //    Это одиночный вложенный объект, который хранится в JSON-колонке.
-            builder.OwnsOne(m => m.KillerDetails, ownedBuilder =>
-            {
-                // .ToJson() указывает EF Core сериализовать/десериализовать весь объект KillerDetails
-                // из колонки "KillerDetails" в базе данных.
-                ownedBuilder.ToJson();
-            });
+            builder.OwnsOne(m => m.KillerDetails, ownedBuilder => ownedBuilder.ToJson());
 
-            // 3. Конфигурируем свойство 'SurvivorDetails'
-            //    Это коллекция вложенных объектов, которая хранится в JSON-колонке.
             builder.OwnsMany(m => m.SurvivorDetails, ownedBuilder =>
             {
-                // 3a. ВАЖНЫЙ ШАГ: Внутри конфигурации для SurvivorDetails мы должны указать,
-                //     что его свойство 'Item' также является вложенным (owned) типом.
-                //     Без этой строки EF Core будет считать 'SurvivorItem' отдельной сущностью и требовать ключ.
                 ownedBuilder.OwnsOne(survivorDetail => survivorDetail.Item);
-
-                // 3b. .ToJson() указывает EF Core сериализовать/десериализовать всю коллекцию SurvivorDetails
-                //     из колонки "SurvivorDetails" в базе данных. Эта команда применится ко всему графу
-                //     объекта SurvivorDetails, включая его вложенный объект Item.
                 ownedBuilder.ToJson();
             });
         }
